48b31af069c73b15c8020a7053cd6fe5476544f4
Taylan Unutmaz <taylanunutmaz@gmail.com>
2020-12-23 02:00:58 +0300

    Add Capsule and Mission models and migrations

---
+++ app/Models/Capsule.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Capsule extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'capsule_serial',
+        'capsule_id',
+        'status',
+        'original_launch',
+        'original_launch_unix',
+        'landings',
+        'details',
+        'reuse_count',
+    ];
+
+    public function missions()
+    {
+        return $this->hasMany(Mission::class);
+    }
+}
